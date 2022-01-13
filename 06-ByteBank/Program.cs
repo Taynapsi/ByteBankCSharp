9e19138a823ebc4040c60ec7b37e25848b852f13
Taynã Souza Malaquias <tayna.malaquias@aliare.co>
2022-01-13 13:19:30 -0300

    Capitulo 5 finalizado! Aprendendo a trabalha com Gets e Sets

---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace _06_ByteBank
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Guilherme";
+            cliente.Cpf = "434.563.879-20";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+       
+    
+}
+
+
