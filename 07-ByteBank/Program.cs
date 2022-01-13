1327cb1886b9cf5651a9be8d471101a17c35f792
Taynã Souza Malaquias <tayna.malaquias@aliare.co>
2022-01-13 14:53:48 -0300

    Capitulo 6 - Get public e Set privado, melhorando nossa segurança/confiança de dados

---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace _07_ByteBank
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente(877, 86712540);
+                      
+
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine("O total de contas é: " + ContaCorrente.TotalDeContasCriadas);
+            Console.ReadLine();
+        }
+    }
+}
